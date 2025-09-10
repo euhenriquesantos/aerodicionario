a1f28623edea6e47123b5cb1b332535d679bb190
Henrique Santos <eu.henriquesantos@icloud.com>
2025-09-10 14:56:36 -0300

    Add Termo model to glossario app

---
+++ glossario/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Termo(models.Model):
+    """Representa um termo do glossário."""
+
+    titulo = models.CharField(max_length=200)
+    definicao = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:  # pragma: no cover - simple human-readable repr
+        return self.titulo
